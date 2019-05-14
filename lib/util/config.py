8f546a3ba9133db4b32f363e2a8526c667b36137
Christopher Welborn <cj@welbornprod.com>
2019-05-13 20:01:37 -0500

    Version fix: Sessions without exit are valid.
    Fixes #8.
    
    I messed up with a version number, and the order
    
    of releases was out of whack.
    
    This should fix it.

--- lib/util/config.py
+++ lib/util/config.py
@@ -38,7 +38,7 @@ __all__ = (
 )
 
 NAME = 'WinCNC-History'
-VERSION = '0.0.5-2'
+VERSION = '0.0.6'
 VERSIONSTR = '{} v. {}'.format(NAME, VERSION)
 AUTHOR = 'Christopher Welborn'
 SCRIPT = os.path.split(os.path.abspath(sys.argv[0]))[1]
